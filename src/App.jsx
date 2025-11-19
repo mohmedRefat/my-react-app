eef4506381e7f98f35a8bd0a26bddbcbaf0b7d6f
Mohmed Refat <mezoxmoonx@gmail.com>
2025-11-19 22:34:42 +0200

    Style in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+{
+  /*
+  *How TO style In react
+  1-Using CSS Modules
+  2-Using Inline Styling
+  3-Using Styled Components
+  4-Using CSS Stylesheets
+  5-Using CSS-in-JS Libraries
+  6-Using Preprocessors (like SASS or LESS)
+  
+  
+  
+  
+  
+*/
+}
 
-import Card from "./Card.jsx"
+import Button from "./Button/Button.jsx";
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Button />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Button/Button.jsx
@@ -0,0 +1,10 @@
+import styles from "./Button.module.css";
+function Button() {
+  return (
+    <div>
+      <button className={styles.button}>Click Me</button>
+    </div>
+  );
+}
+
+export default Button;
