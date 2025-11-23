03e54a1fe48eb0d451aae27e54f67527bcafa90f
Mohmed Refat <mezoxmoonx@gmail.com>
2025-11-23 20:27:43 +0200

    on change ex on form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import Counter from "./Counter";
+/*
+*onChange = is event handler used primary especially with form Elements liks
+*select textarea input 
+*trigger function every time value exhanged 
+
+*/
+import MyCompo from "./MyCompo";
 function App() {
   return (
     <>
-      <Counter />
+      <MyCompo />
     </>
   );
 }
