83461ffd7b04f93860002291b1f3a40607ffe033
Mohmed Refat <mezoxmoonx@gmail.com>
2025-12-06 22:41:45 +0200

    first lesson on useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+/*
+*use effect like use sideway code (function,[dependecies])
+
+1. useEffect(()=>{}) Runs after every re-render
+2. useEffect(()=>{},[]) Runs only  on mount 
+3. useEffect(()=>{},[value]) Runs on mount + when value change 
+
+
+*/
 
 import MyCompo from "./MyCompo";
 function App() {
--- src/MyCompo.jsx
+++ src/MyCompo.jsx
@@ -1,47 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function MyCompo() {
-  const [food, setFood] = useState(["apple", "banana", "orange"]);
-
-  function handleAddElement() {
-    const newFood = document.getElementById("inp").value;
-    document.getElementById("inp").value = "";
-    setFood((f) => [...f, newFood]);
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
+  function add() {
+    setCount((c) => c + 1);
+  }
+  function Subtract() {
+    setCount((c) => c - 1);
+  }
+  function ChangeColor() {
+    setColor(color === "green" ? "red" : "green");
   }
 
-  function handleDelete(index) {
-    setFood(food.filter((_, i) => i !== index));
-  }
+  useEffect(() => {
+    document.title = `count is ${count} `;
+  }, [color]);
 
   return (
-    <div className="max-w-sm mx-auto mt-10 p-6 bg-white rounded-2xl shadow-lg">
-      <h2 className="text-center text-2xl font-semibold mb-4">list of food</h2>
-
-      <ul className="mb-4">
-        {food.map((food, index) => (
-          <li
-            key={index}
-            onClick={() => handleDelete(index)}
-            className="bg-gray-100 mb-2 px-4 py-2 rounded-lg cursor-pointer transition hover:bg-red-100 hover:translate-x-1"
-          >
-            {food}
-          </li>
-        ))}
-      </ul>
-
-      <div className="flex gap-2">
-        <input
-          type="text"
-          id="inp"
-          className="flex-1 px-3 py-2 border rounded-lg outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-        />
-        <button
-          onClick={handleAddElement}
-          className="px-4 py-2 bg-blue-600 text-white rounded-lg transition hover:bg-blue-700"
-        >
-          Add
-        </button>
-      </div>
+    <div>
+      <p style={{ color: color }}>{count}</p>
+      <button onClick={add}>Add</button>
+      <button onClick={Subtract}>Subtract</button>
+      <button onClick={ChangeColor}>ChangeColor</button>
     </div>
   );
 }
